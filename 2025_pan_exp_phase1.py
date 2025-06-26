3f872960792e9799354a299b57596bc23ea675f4
Steffi Kim <steffikim@ucsc-guest-169-233-124-35.ucsc.edu>
2025-06-26 16:48:10 -0700

    basic draft

--- 2025_pan_exp_phase1.py
+++ 2025_pan_exp_phase1.py
@@ -3,6 +3,48 @@
 """
 Created on Thu Jun 26 11:31:18 2025
 
+
 @author: joncparamore
 """
+
+
+#PHASE 1: Baseline Code. Should display one word at a time and a timer, instructing participants to read word aloud as fastly and clearly as possible. 
+
+#Import Library for interface - maybe use a different library than tkinter?
+
+import tkinter as tk
+import time
+
+
+
+#Set up GUI
+
+root = tk.Tk()
+root.title("Baseline Window")
+root.geometry("300x200")
+root.mainloop()
+
+#Import wordlist
+
+#Create Timer
+
+
+#Set up point system for fast responses
+
+points = 0
+
+
+
+#Instruction screen?
+
+
+#Loop that displays each word, runs the timer for each word, awards points for each word, writes messages
+
+
+
+
+#Random messages indicating the speech was not fast/clear enough
+
+
+
 
