024480a649fee86e903c83fc8d6deaab79351303
Steffi Kim <steffikim@college9-169-233-122-220.resnet.ucsc.edu>
2025-06-30 11:28:03 -0700

    Baseline Phase first draft

--- 2025_pan_exp_phase1.py
+++ 2025_pan_exp_phase1.py
@@ -7,44 +7,165 @@ Created on Thu Jun 26 11:31:18 2025
 @author: joncparamore
 """
 
-
-#PHASE 1: Baseline Code. Should display one word at a time and a timer, instructing participants to read word aloud as fastly and clearly as possible. 
-
-#Import Library for interface - maybe use a different library than tkinter?
-
-import tkinter as tk
-import time
-
-
-
-#Set up GUI
-
-root = tk.Tk()
-root.title("Baseline Window")
-root.geometry("300x200")
-root.mainloop()
-
-#Import wordlist
-
-#Create Timer
+#Imports
+import sys
+from PyQt5.QtWidgets import (
+   QApplication, QWidget, QLabel, QPushButton
+)
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QTimer, Qt
 
 
-#Set up point system for fast responses
+app = QApplication([])
 
+#Wordlist Variables
+word_num = 0
+word_list = ["ant", "bear", "cat", "dog", "elephant", "flamingo", "goat", "horse", "iguana", "jellyfish", "koala"]
+
+
+
+#Create Window
+window = QWidget()
+window.setWindowTitle("Phase 1: Baseline Window")
+window.setGeometry(100,100,800,500)
+
+#Create Main Text Label
+
+current_word = QLabel("Phase 1: Baseline Reading", window)
+current_word.setFont(QFont("Verdana", 34))
+current_word.adjustSize()
+current_word.move((window.width()-current_word.width())//2, 100)
+
+
+#Create Instructions for the first screen only
+
+instructions = QLabel("This phase will test your language speaking abilities. You will see a series of words on the screen. Read each word aloud as fastly and clearly as possible.", window)
+instructions.setFont(QFont("Verdana", 15))
+instructions.setWordWrap(True)
+instructions.setFixedWidth(400)
+instructions.adjustSize()
+instructions.move(window.width()//2-instructions.width()//2, 200)
+
+
+#Create Next Button
+next_button = QPushButton("Next", window)
+next_button.adjustSize()
+next_button.move(window.width()//2-next_button.width()//2, 300)
+next_button.setStyleSheet("background-color: lightblue")
+
+
+
+#Setup Timer
+timer=QTimer()
+timer.setInterval(100)      #Use 1 decisecond = 100 milliseconds
+elapsed_deciseconds = 0
+
+clock = QLabel("", window)
+clock.setAlignment(Qt.AlignCenter)
+clock.resize(95, 30)  
+clock.setFont(QFont("Verdana", 17))
+clock.setStyleSheet("background-color: lightgrey; border: 2 px; padding: 6px; border-radius: 5px")
+clock.move(window.width()//2-clock.width()//2, 100)
+clock.hide()
+
+
+def update_time():
+    global elapsed_deciseconds
+    elapsed_deciseconds +=1
+    seconds = elapsed_deciseconds / 10
+    clock.setText(f"{seconds:01}" + "   sec")
+
+
+timer.timeout.connect(update_time) #After every decisecond passes, the update_time function is called
+actually_timed = False      #Used to make sure the first word times correctly
+
+
+#Create Points
 points = 0
-
-
-
-#Instruction screen?
-
-
-#Loop that displays each word, runs the timer for each word, awards points for each word, writes messages
+point_popup = QLabel("", window)
+point_popup.setAlignment(Qt.AlignCenter)
+point_popup.resize(80, 50)  
+point_popup.setFont(QFont("Verdana", 25))
 
 
 
 
-#Random messages indicating the speech was not fast/clear enough
+
+#Function that works the program, triggered whenever the Next button is clicked. Adjusts timer and points
+
+def setup_next_word():
+    global elapsed_deciseconds, current_word, points, word_num, actual_time
+    instructions.hide()
+    timer.stop()
+    point_popup.move(window.width()//2 + current_word.width()//2+50, 202)
+    if elapsed_deciseconds<12 and actually_timed is True:
+            points += 50
+            point_popup.setText("+50")
+            point_popup.setStyleSheet("color:green")
+            QTimer.singleShot(1000, display_next_word) 
+    elif elapsed_deciseconds<20 and actually_timed is True:
+            points += 20
+            point_popup.setText("+20")
+            point_popup.setStyleSheet("color:DarkOliveGreen")
+            QTimer.singleShot(1000, display_next_word) 
+    elif elapsed_deciseconds<30 and actually_timed is True:
+            points += 5
+            point_popup.setText("+5")
+            point_popup.setStyleSheet("color: DarkGoldenrod")
+            QTimer.singleShot(1000, display_next_word) 
+    elif actually_timed is True:
+            points += 0
+            point_popup.setText("+0")
+            point_popup.setStyleSheet("color: FireBrick")
+            QTimer.singleShot(1000, display_next_word) 
+    else:
+        display_next_word()
+    
+
+#Function that causes each new word to be displayed
+
+def display_next_word():
+    point_popup.setText("")
+    global elapsed_deciseconds, current_word, points, word_num, actually_timed
+    elapsed_deciseconds=0
+    
+    
+    if word_num < len(word_list):
+        current_word.setText(word_list[word_num]) 
+        current_word.setFont(QFont("Verdana", 44))
+        current_word.adjustSize()
+        current_word.move((window.width()-current_word.width())//2, 200)
+        word_num+=1
+
+        clock.show()
+        clock.setText("0.0   sec")
+        timer.start()
+        actually_timed = True
+
+    else:
+        timer.stop()
+        clock.hide()
+        next_button.hide()
+        current_word.setText("All finished!")
+        current_word.adjustSize()
+        current_word.move((window.width()-current_word.width())//2, 200)
+        window.final_points = QLabel("Total Points: " + str(points),window)
+        window.final_points.setFont(QFont("Verdana", 17))
+        window.final_points.setStyleSheet("background-color: lightgreen; border: 2 px; padding: 6px; border-radius: 5px")
+        window.final_points.adjustSize()
+        window.final_points.move((window.width()-window.final_points.width())//2, 100)
+        window.final_points.show()
 
 
+    
+
+#Next button triggers the functions, causes the program to run
+
+next_button.clicked.connect(setup_next_word)
+
+
+
+window.show()
+app.exec_()
 
 
