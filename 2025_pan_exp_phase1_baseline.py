7814a8e08b86dd5ab2ddf730b25b2e64c19e562e
SteffiK12 <skim452@ucsc.edu>
2025-07-08 11:29:28 -0700

    Update 2025_pan_exp_phase1_baseline.py

--- 2025_pan_exp_phase1_baseline.py
+++ 2025_pan_exp_phase1_baseline.py
@@ -437,7 +437,7 @@ def display_next_word():
       layout.addSpacerItem(QSpacerItem(0, 540, QSizePolicy.Minimum, QSizePolicy.Preferred)) 
 
       filename = f"{user_id}_phase1_baseline_word_order.csv"   #Download the .csv of all of the words once you have reached the end of the list
-      with open(filename, 'w', newline='') as file:
+      with open(filename, 'w', newline='', encoding='utf-8-sig') as file:
             writer = csv.writer(file)
             for word in phase1_word_order_list:
                 writer.writerow([word])
