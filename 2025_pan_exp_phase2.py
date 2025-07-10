f36faab1a5f8d3f943162d4dcb5bfbe8147e72f6
GinaL <rrrlim2@gmail.com>
2025-07-10 11:45:00 -0700

    Update 2025_pan_exp_phase2.py

--- 2025_pan_exp_phase2.py
+++ 2025_pan_exp_phase2.py
@@ -201,7 +201,7 @@ class TileGame(QWidget):
         self.point_countdown.hide()
 
         self.points_label = QLabel("", self)
-        self.points_label.setFont(QFont("Verdana", self.scale_w(0.025)))
+        self.points_label.setFont(QFont("Verdana", self.scale_h(0.025)))
         self.points_label.resize(700, 40)
         self.points_label.move((self.screen_width - 500) // 2, self.scale_h(0.18))
 
