f8502eb91138a0608268f1e4c43b62967cef48d7
GinaL <rrrlim2@gmail.com>
2025-07-10 09:47:40 -0700

    Update 2025_pan_exp_phase3_shadow.py

--- 2025_pan_exp_phase3_shadow.py
+++ 2025_pan_exp_phase3_shadow.py
@@ -41,7 +41,7 @@ app = QApplication([])
 class EscapeWindow(QWidget):
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            self.showNormal()  # Exit fullscreen
+            self.showMaximizedl()  # Exit fullscreen
 
 
 
